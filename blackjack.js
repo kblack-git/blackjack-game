d1e9a4d1ad5e827dc84ac8686ea8e5caec5f1714
Keith Black <keithblack@keiths-air.attlocal.net>
2023-06-24 14:32:51 -0400

    display card images

--- blackjack.js
+++ blackjack.js
@@ -1,3 +1,5 @@
+import { cardDeck } from './cardDeck.js'
+
 /*     declare variables for a random numbers, array to hold 52 random rumbers, and 
        array to represent a deck of cards */
 
@@ -13,6 +15,7 @@ let dealerScore = document.querySelector('.dealerCount')
 let dealerScore2 = document.querySelector('.dealerCount2')
 let deckCardsLeft = document.querySelector('.deckCardsLeft')
 let winLoseBust = document.querySelector('.winLoseBust')
+let hitButton = document.querySelector('#hitButton')
 
 let shuffledDeck = []
 let dealerHand = []
@@ -27,6 +30,12 @@ let hitOrStand=''
 let aceCount
 let existingAce=false
 let card
+let cardsLeft
+let cardsLeftAfterDeal
+let numberOfCards
+let player
+
+
 
 // const prompt = require("prompt-sync")();
 // const input = prompt("Ready to play blackjack?");
@@ -35,13 +44,43 @@ function fisherYatesShuffle() {
     let cardsArray = [2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,
         9,9,9,9,10,10,10,10,'j','j','j','j','q','q','q','q','k','k','k',
         'k','a','a','a','a']
-    let i = cardsArray.length;
-    while (--i > 0) {
+    let i = cardDeck.length;
+        while (--i > 0) {
        let randomIndex = Math.floor(Math.random() * (i + 1));
-       [cardsArray[randomIndex], cardsArray[i]] = [cardsArray[i], cardsArray[randomIndex]];
+       [cardDeck[randomIndex], cardDeck[i]] = [cardDeck[i], cardDeck[randomIndex]];
     }
-shuffledDeck=cardsArray
+shuffledDeck=cardDeck
+
 }
+// shuffledDeck=[
+    
+//         {
+//             card: '2h',
+//             value: 2,
+//             image:'./images/2_of_hearts.png'
+//         },
+//         {
+//             card: '2d',
+//             value: 2,
+//             image: './images/2_of_diamonds.png'
+//         },
+//         {
+//             card: '2c',
+//             value: 2,
+//             image: './images/2_of_clubs.png'
+//         },
+//         {
+//             card: '2s',
+//             value: 2,
+//             image: './images/2_of_spades.png'
+//         },
+//         {
+//             card: '3s',
+//             value: 3,
+//             image: './images/3_of_spades.png'
+//         }
+// ]
+
 
 // Function tp deal cards with parameter for # of cards to deal
 function dealCard(numberOfCards, playerOrDealer) {
@@ -50,16 +89,10 @@ function dealCard(numberOfCards, playerOrDealer) {
     cardsLeftAfterDeal = cardsLeft-numberOfCards
     while (--cardsLeft>=cardsLeftAfterDeal) {
         playerOrDealer.push(shuffledDeck.shift())
-        
-
-        // shuffledDeck.shift()
-        
-        // card = cardsArray[randomArray[randomArray.length-1]]
-        // return card
-        
     }
     return(playerOrDealer)
 }
+    
 
 
 // Function to count dealt cards
@@ -71,25 +104,25 @@ function countDealtCards(handToCount, aceCount) {
     count2=0
     numberOfCards=handToCount.length
     console.log(numberOfCards)
-    while (--numberOfCards>=0 && count2<22) {
-        if (typeof(handToCount[numberOfCards])==='string') {
-            if (handToCount[numberOfCards]==='j'|| handToCount[numberOfCards]==='q'||handToCount[numberOfCards]==='k') {
+    while (--numberOfCards>=0) {
+        if (typeof(handToCount[numberOfCards].value)==='string') {
+            if (handToCount[numberOfCards].value==='j'|| handToCount[numberOfCards].value==='q'||handToCount[numberOfCards].value==='k') {
                 count += 10
                 count2 += 10
-            } else if (handToCount[numberOfCards]==='a' && !existingAce) {
+            } else if (handToCount[numberOfCards].value==='a' && !existingAce) {
                 existingAce=true
                 count += 1;
                 count2 += 11;
-            } else if (handToCount[numberOfCards]==='a' && existingAce) {
+            } else if (handToCount[numberOfCards].value==='a' && existingAce) {
                 count += 1
                 count2 += 1
             } 
         } else {
-                count += handToCount[numberOfCards] 
-                count2 += handToCount[numberOfCards]
+                count += handToCount[numberOfCards].value 
+                count2 += handToCount[numberOfCards].value
             }
             console.log(count,count2)
-        }
+    }
     // if (count<21 || count2<21) {
     //     return (aceCount===1? count : count2)
     // } else if (count===21 || count2===21) {
@@ -131,21 +164,30 @@ function displayScores() {
     }
 }
 
+
 //deal player another card when 'hit' button clicked and update page
+
 function hitCard() {
     dealCard(1,playerHand)
-    playerCards.innerHTML=playerHand
+    // playerCards.innerHTML=playerHand
+    displayHand(playerHand, playerCards)
     displayScores()
     // playerScore.innerHTML=('Player Count : ' + countDealtCards(playerHand))
     deckCardsLeft.innerHTML=('Cards left: '+ shuffledDeck.length)
-
 }
 
-function isWinner() {
-    
+function displayHand (pOrDHand, pOrDCards) {
+    while (pOrDCards.firstChild) {
+        pOrDCards.removeChild(pOrDCards.firstChild);
+      }
+    pOrDHand.map(a => {
+        let image = document.createElement("img")
+        pOrDCards.appendChild(image)
+        image.src = `${a.image}`
+    })    
 }
-    
-    
+
+hitButton.addEventListener("click",hitCard)
 
 
 
@@ -156,15 +198,20 @@ fisherYatesShuffle()
 // deal and display dealer's & player's first 2 cards
 dealCard(2, playerHand)
 dealCard(2, dealerHand)
-playerCards.innerHTML=playerHand
-dealerCards.innerHTML=dealerHand
+
+displayHand(playerHand, playerCards)
+displayHand(dealerHand, dealerCards)
+
+
+// playerCards.innerHTML=playerHand
+// dealerCards.innerHTML=dealerHand
 displayScores()
 
 //display # of cards left in deck in header
 deckCardsLeft.innerHTML=('Cards left: '+ shuffledDeck.length)
 
 
-
+console.log(cardDeck)
 
 
 
